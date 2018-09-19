d44a871e627b19e3252df61dcc69d4fa62d62280
許銘仁 <surprised128@gmail.com>
2018-09-19 16:00:52 +0800

    2018-09-19

--- src/main/java/ntou/mingjen/rest/parse/EndpointLevel.java
+++ src/main/java/ntou/mingjen/rest/parse/EndpointLevel.java
@@ -15,20 +15,49 @@ public class EndpointLevel {
                 boolean checkRestful = p.contains("{");
                 String endpoint = baseUrl+p;
                 io.swagger.models.Operation swaggerOperation = swagger.getPaths().get(p).getDelete();
-
+                if(checkRestful){
+                    feture.add("REST-style URls");
+                }
             }
             if (swagger.getPaths().get(p).getGet() != null) {
+                boolean checkRestful = p.contains("{");
+                String endpoint = baseUrl+p;
                 io.swagger.models.Operation swaggerOperation = swagger.getPaths().get(p).getGet();
+                if(checkRestful){
+                    feture.add("REST-style URls");
+                }
             }
             if (swagger.getPaths().get(p).getPatch() != null) {
+                boolean checkRestful = p.contains("{");
+                String endpoint = baseUrl+p;
                 io.swagger.models.Operation swaggerOperation = swagger.getPaths().get(p).getPatch();
+                if(checkRestful){
+                    feture.add("REST-style URls");
+                }
             }
             if (swagger.getPaths().get(p).getPost() != null) {
+                boolean checkRestful = p.contains("{");
+                String endpoint = baseUrl+p;
                 io.swagger.models.Operation swaggerOperation = swagger.getPaths().get(p).getPost();
+                if(checkRestful){
+                    feture.add("REST-style URls");
+                }
             }
             if (swagger.getPaths().get(p).getPut() != null) {
+                boolean checkRestful = p.contains("{");
+                String endpoint = baseUrl+p;
                 io.swagger.models.Operation swaggerOperation = swagger.getPaths().get(p).getPut();
+                if(checkRestful){
+                    feture.add("REST-style URls");
+                }
             }
         }
+    }
+
+    public ArrayList<String> checkStatusCodeAndErrorMessages(io.swagger.models.Operation swaggerOperation, ArrayList<String> feture){
+        for(String key : swaggerOperation.getResponses().keySet()){
+            io.swagger.models.Response response = swaggerOperation.getResponses().get(key);
+        }
+        return null;
     }
 }
